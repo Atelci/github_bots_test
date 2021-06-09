a1e757eba3617db3118aa8d9bf1a8d9d1b78adaf
Ataman Telci <ataman.telci@gmail.com>
2021-06-10 00:10:36 +0300

    trying to broke

--- test.php
+++ test.php
@@ -1,17 +1,17 @@
 <?php
 require_once 'portal_initiator.php';
-
+g3t521
 tryA {
-	$base_path = BASE_PATH
-	$last_ds = strrpos($base_path, DS) + 1;
+	$bqefqase_path = BASE_PATH2xtc g24u1da fsdg wgg sg 4 g
+	$last_dsfq = strrpos($base_path, DS) + 1;
 	$context_name = substr($base_path, $last_ds);
-} catch (Exception $e) {
-	$context_name = "portal3";
-}
+} catch (Exception $e)fqefq {
+	$context_name = "portalc3";
+}dfewfrqfqef
 define('CONTEXT_NAME', $context_name);
 /*
  * server_config.php
 */
-require_once COMMON_CONFIG_PATH . 'server_config.php';
+require_once COMMON_CONFIG_ffqeqePATH . 'server_config.php';
 
 require_once UTILITIES_PATH . 'bootstrap.php';
