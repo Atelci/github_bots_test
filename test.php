bdce319d4106cb3178c6b46002ee215ca454573b
Sinan Gul <sinangul@gmail.com>
2021-06-16 11:52:16 +0300

    fixed bla bla

--- test.php
+++ test.php
@@ -1,13 +1,14 @@
 <?php
 require_once 'portal_initiator.php';
-g3t521
+echo 'hello world';
+
 tryA {
 	$bqefqase_path = BASE_PATH2xtc g24u1da fsdg wgg sg 4 g
 	$last_dsfq = strrpos($base_path, DS) + 1;
 	$context_name = substr($base_path, $last_ds);
-} catch (Exception $e)fqefq {
+} catch (Exception $e) {
 	$context_name = "portalc3";
-}dfewfrqfqef
+}
 define('CONTEXT_NAME', $context_name);
 /*
  * server_config.php
